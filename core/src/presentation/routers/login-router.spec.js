6840c1f30513b6fc4e3b0741b8b090a38bbd3f06
mrvortexx <matheus034@yahoo.com>
2021-10-30 01:12:16 +0000

    improve login router test

--- core/src/presentation/routers/login-router.spec.js
+++ core/src/presentation/routers/login-router.spec.js
@@ -1,6 +1,10 @@
 class LoginRouter {
     async route(httpRequest){
-        const {password, email} = httpRequest;
+
+        if(!httpRequest || !httpRequest.body){
+            return this.response(500, {message: "Internal error."})
+        }
+        const {password, email} = httpRequest.body;
         if(!password || !email){
             return this.response(400, {message: "Email or password is not provided."})
         }
@@ -15,15 +19,25 @@ class LoginRouter {
 }
 
 describe('Login router', () => {
+    test('Should return 500 if httpRequest has not body.', async () => {
+        const loginRouter = new LoginRouter();
+        const response = await loginRouter.route();
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body.message).toBe("Internal error.");
+    });
+
     test('Should return 400 if  email or password is not provided', async () => {
         const loginRouter = new LoginRouter();
         const httpRequest = {
-
+            body:{
+                
+            }
         }
         const response = await loginRouter.route(httpRequest);
 
         expect(response.statusCode).toBe(400);
         expect(response.body.message).toBe("Email or password is not provided.");
-        
     });
+    
 })
